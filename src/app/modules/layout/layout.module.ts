aec8d74ca9d546e29122ef4859a3b1f520c72949
saksham <saksham.09.gupta@gmail.com>
2022-04-04 12:15:14 +0530

    WIP : App intro

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -5,10 +5,9 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { AboutComponent } from './components/about/about.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [LayoutComponent, IntroComponent, AboutComponent],
-  imports: [CommonModule, FontAwesomeModule, LayoutRoutingModule],
+  imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
