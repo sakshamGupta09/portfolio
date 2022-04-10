4bf8dcc784f95810028191666851365bb76de1fd
saksham <saksham.09.gupta@gmail.com>
2022-04-10 06:44:01 +0530

    about section done

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -6,9 +6,10 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './components/layout/layout.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
 
 @NgModule({
-  declarations: [LayoutComponent, IntroComponent, AboutComponent],
+  declarations: [LayoutComponent, IntroComponent, AboutComponent, ExperiencesComponent],
   imports: [CommonModule, FontAwesomeModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
